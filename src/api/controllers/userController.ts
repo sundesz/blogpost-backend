c85f40d365298df5cab6e3fc913798aa45748fc8
Sandesh Hyoju <sundesz@gmail.com>
2023-07-01 23:31:39 +0300

    Change user controller and router to upload image

--- src/api/controllers/userController.ts
+++ src/api/controllers/userController.ts
@@ -1,29 +1,77 @@
 import { NextFunction, RequestHandler } from 'express';
 import bcrypt from 'bcrypt';
+import crypto from 'node:crypto';
+import fs from 'fs';
 import { SALT } from '../../config';
-import { User } from '../../db/models';
+import { Image, User } from '../../db/models';
 import { userMessage } from '../../utils/userMessages';
 import { Sequelize } from 'sequelize';
 import { NewUserParams } from '../../types';
+import sharp from 'sharp';
+import createHttpError from 'http-errors';
+import { SRC_DIR } from '../../index';
 
 /**
  * Create user
  */
-const create: RequestHandler<unknown, unknown, NewUserParams> = async (
+const create: RequestHandler<unknown, unknown, NewUserParams, unknown> = async (
   req,
   res,
   next: NextFunction
 ) => {
   try {
     const { name, email, password, role } = req.body;
+    const userId = crypto.randomUUID();
+    const profilePic = req.file;
+
+    // Check if user already exist
+    const isUserExists = await User.count({ where: { email } });
+    if (isUserExists) {
+      throw createHttpError(409, userMessage.warning.USERNAME_ALREADY_EXISTS);
+    }
+
+    // Insert image
+    let profilePicDestinationPath: string | undefined = undefined;
+    let imageData: Image | null = null;
+
+    if (profilePic) {
+      const dir = `${SRC_DIR}/../uploads/profile_pictures`;
+
+      if (!fs.existsSync(dir)) {
+        await fs.promises.mkdir(dir, { recursive: true });
+      }
+
+      profilePicDestinationPath = `/uploads/profile_pictures/${userId}.png`;
+
+      await sharp(profilePic.buffer)
+        .resize(200, 200, { withoutEnlargement: true, fit: sharp.fit.contain })
+
+        .png({
+          // https://sharp.pixelplumbing.com/api-output#png
+          compressionLevel: 8,
+          palette: true,
+          quality: 75, // play around with this number until you get the file size you want
+        })
+        .toFile('./' + profilePicDestinationPath);
+
+      imageData = await Image.create({
+        name: userId,
+        originalName: profilePic.originalname,
+        fileLocation: profilePicDestinationPath,
+      });
+    }
+
+    // Insert User
     const passwordHash = await bcrypt.hash(password, Number(SALT));
 
     await User.create({
+      userId,
       email,
       name,
       passwordHash,
       isActive: true,
       role,
+      imageId: imageData ? imageData.imageId : imageData,
     });
 
     res.status(201).json(userMessage.info.USER_CREATED);
--- src/api/routers/userRouter.ts
+++ src/api/routers/userRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { isAdmin } from '../../middleware/helper';
 import userController from '../controllers/userController';
+import { profilePicUpload } from '../../middleware/imageUpload';
 const userRouter = Router();
 
-userRouter.post('/', userController.create);
+userRouter.post('/', profilePicUpload.single('image'), userController.create);
 userRouter.put('/toggle/:id', isAdmin, userController.toggle);
 
 export default userRouter;
