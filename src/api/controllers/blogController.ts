13ec042d67205577d564453f9745f8e299e744be
Sandesh Hyoju <sundesz@gmail.com>
2023-07-04 01:04:56 +0300

    Add endpoint to upload image for blog

--- src/api/controllers/blogController.ts
+++ src/api/controllers/blogController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, RequestHandler } from 'express';
 import { sequelize } from '../../db';
-import { Blog, Comment, Rating, User } from '../../db/models';
+import crypto from 'node:crypto';
+import { Blog, Comment, Image, Rating, User } from '../../db/models';
 import {
   AllQueryParams,
   NewBlogParams,
@@ -8,7 +9,10 @@ import {
   UpdateBlogParams,
 } from '../../types';
 import { getPagination, getPagingData } from './helper';
-import { Op } from 'sequelize';
+import { Op, literal } from 'sequelize';
+import fs from 'fs';
+import { SRC_DIR } from '../../index';
+import sharp from 'sharp';
 
 /**
  * Reaction count subquery
@@ -153,7 +157,13 @@ const getAllBlogs: RequestHandler<
       attributes: [
         'blogId',
         'title',
-        [sequelize.fn('LEFT', sequelize.col('content'), 50), 'content'], // Return first n characters in the string
+        [
+          literal(
+            "substring(regexp_replace(content, '<[^>]+>', '', 'g'), 1, 50)"
+          ),
+          'content',
+        ],
+        // [sequelize.fn('LEFT', sequelize.col('content'), 50), 'content'], // Return first n characters in the string
         'slug',
       ],
       include: { model: User, attributes: ['name', 'email', 'userId'] },
@@ -267,10 +277,56 @@ const toggle: RequestHandler = async (req, res, next: NextFunction) => {
   }
 };
 
+const uploadImage: RequestHandler = async (req, res, next: NextFunction) => {
+  try {
+    const blogImage = req.file;
+
+    // Insert image
+    let inPostImageDestinationPath: string | undefined = undefined;
+    const imageId = crypto.randomUUID();
+
+    if (blogImage) {
+      const dir = `${SRC_DIR}/../uploads/blogs`;
+
+      if (!fs.existsSync(dir)) {
+        await fs.promises.mkdir(dir, { recursive: true });
+      }
+
+      inPostImageDestinationPath = `/uploads/blogs/${imageId}.png`;
+
+      await sharp(blogImage.buffer)
+        .resize(800, 600, { withoutEnlargement: true, fit: sharp.fit.contain })
+
+        .png({
+          // https://sharp.pixelplumbing.com/api-output#png
+          compressionLevel: 7,
+          palette: true,
+          quality: 85, // play around with this number until you get the file size you want
+        })
+        .toFile('./' + inPostImageDestinationPath);
+
+      await Image.create({
+        name: imageId,
+        originalName: blogImage.originalname,
+        fileLocation: inPostImageDestinationPath,
+      });
+
+      // return res.json({ imageId: imageData.dataValues.imageId });
+      const imageUrl = `http://localhost:8080/images/blogs/${imageId}.png`;
+      res
+        .status(200)
+        .json({ uploaded: 1, fileName: `${imageId}.png`, url: imageUrl }); // Send the image URL in the response
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
 export default {
   getBlog,
   getAllBlogs,
   create,
   update,
   toggle,
+  uploadImage,
 };
--- src/api/routers/blogRouter.ts
+++ src/api/routers/blogRouter.ts
@@ -6,11 +6,17 @@ import {
   isAuthor,
 } from '../../middleware/helper';
 import blogController from '../controllers/blogController';
+import { inPostImageUpload } from '../../middleware/imageUpload';
 
 const blogRouter = Router();
 
 blogRouter.get('/', blogController.getAllBlogs);
 blogRouter.get('/:slug', blogController.getBlog);
+blogRouter.post(
+  '/blogImage',
+  inPostImageUpload.single('upload'),
+  blogController.uploadImage
+);
 blogRouter.post('/', isAuthor, blogController.create);
 blogRouter.put('/toggle/:id', isAdmin, blogFinder, blogController.toggle);
 blogRouter.put('/:id', isAdminOrAuthor, blogFinder, blogController.update);
