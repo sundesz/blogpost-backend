4beebc367d04398dc64313cc6db7928b5f86d0dd
Sandesh Hyoju <sundesz@gmail.com>
2023-07-07 23:31:12 +0300

    Add where condition to getAuthor

--- src/api/controllers/authorController.ts
+++ src/api/controllers/authorController.ts
@@ -1,9 +1,21 @@
 import { NextFunction, RequestHandler } from 'express';
 import { Blog, User } from '../../db/models';
-import { sequelize } from '../../db';
 import { AllQueryParams, IdParams } from '../../types';
-import { Op } from 'sequelize';
+import { Op, literal } from 'sequelize';
 import { getPagination, getPagingData } from './helper';
+import { sequelize } from '../../db';
+
+const blogCountSubQuery = () => {
+  // TODO: pass has access to show blog.published = true
+  return sequelize.literal(`(
+    SELECT COUNT(*)::int
+    FROM blogs
+    WHERE
+        blogs.user_id = "User".user_id
+        AND
+        blogs.published = true
+    )`);
+};
 
 /**
  * Get author
@@ -16,21 +28,33 @@ const getAuthor: RequestHandler<IdParams> = async (
   try {
     const { id: userId } = req.params;
 
+    let where = {};
+    where = { published: true };
+    if (req.session.data !== undefined) {
+      const { role: sessionRole, userId: sessionUserId } = req.session.data;
+      where = sessionRole === 'admin' || sessionUserId === userId ? {} : where;
+    }
+
     const author = await User.findOne({
       attributes: ['email', 'name', 'userId', 'imageId'],
       where: { userId, isActive: true, role: 'author' },
       include: {
         model: Blog,
-
         attributes: [
           'blogId',
           'title',
-          [sequelize.fn('LEFT', sequelize.col('content'), 50), 'content'], // Return first n characters in the string
+          [
+            literal(
+              "substring(regexp_replace(content, '<[^>]+>', '', 'g'), 1, 50)"
+            ),
+            'content',
+          ],
           'slug',
           'updatedAt',
+          'published',
         ],
         required: false,
-        where: { published: true },
+        where,
       },
     });
 
@@ -71,21 +95,19 @@ const getAllAuthor: RequestHandler<
       };
     }
 
-    // where: { role: 'author', isActive: true },
-
     if (orderBy && orderDir) {
       order = [[String(orderBy), String(orderDir)]];
     }
 
     const { limit, offset } = getPagination(pageNumber);
     const authorsData = await User.findAndCountAll({
-      attributes: ['email', 'name', 'userId', 'imageId'],
-      include: {
-        model: Blog,
-        attributes: ['blogId'],
-        required: false,
-        where: { published: true },
-      },
+      attributes: [
+        'email',
+        'name',
+        'userId',
+        'imageId',
+        [blogCountSubQuery(), 'blogCount'],
+      ],
       where,
       order,
       offset,
